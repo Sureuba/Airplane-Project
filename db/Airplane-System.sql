e41456268881ff5b789c6aad1b4977d3f13613fc
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2023-11-24 16:30:24 -0700

    DB

---
+++ db/Airplane-System.sql
@@ -0,0 +1,66 @@
+CREATE TABLE Passenger (
+    PassengerID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    ContactInfo VARCHAR(100),
+    Address VARCHAR(100),
+    Email VARCHAR(100),
+    MemberStatus ENUM('Guest', 'Member')
+);
+
+CREATE TABLE PaymentInfo (
+    MemberID INT PRIMARY KEY,
+    CardNumber VARCHAR(16),
+    CardExpYear INT(4),
+    CardCSV INT(3),
+    FOREIGN KEY (MemberID) REFERENCES Passenger(PassengerID)
+);
+
+CREATE TABLE Admin(
+	AdminID INT PRIMARY KEY
+);
+
+CREATE TABLE Flights (
+    FlightID INT PRIMARY KEY,
+    DepartureLocation VARCHAR(100),
+    Destination VARCHAR(100),
+    DepartureTime DATETIME,
+    ArrivalTime DATETIME,
+    SeatNumber VARCHAR(10),
+    IsAvailable BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (AircraftID) REFERENCES Aircraft(AircraftID),
+    FOREIGN KEY (CrewID) REFERENCES Crew(CrewID)
+);
+
+CREATE TABLE FlightSeats (
+    SeatID INT AUTO_INCREMENT PRIMARY KEY,
+    SeatType ENUM('Business', 'Ordinary', 'Comfort')
+);
+
+CREATE TABLE Reservations (
+    ReservationID INT PRIMARY KEY,
+    PassengerID INT, -- Foreign Key to Passengers
+    FlightID INT, -- Foreign Key to Flights
+	ReservationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	IsCancelled BOOLEAN DEFAULT FALSE,
+    TicketInsurance BOOL,
+	FOREIGN KEY (PassengerID) REFERENCES Passenger(PassengerID),
+    FOREIGN KEY (FlightID) REFERENCES Flight(FlightID)
+);
+
+CREATE TABLE Aircraft (
+    AircraftID INT PRIMARY KEY,
+    Model VARCHAR(100),
+    Capacity INT,
+    AircraftCondition BOOL 
+);
+
+CREATE TABLE Crew (
+    CrewID INT PRIMARY KEY,
+    CrewName VARCHAR(100)
+);
+
+
+GRANT INSERT, DELETE ON Crew TO AdminRole;
+GRANT INSERT, DELETE ON Aircraft TO AdminRole;
+GRANT INSERT, DELETE ON Flights TO AdminRole;
+
