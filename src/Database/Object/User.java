bfa3225e669445a77e501fcc65ed8c0286f3e39d
Alexeygrekov <94085810+Alexeygrekov@users.noreply.github.com>
2023-11-28 09:02:32 -0700

    Update User.java

--- src/Database/Object/User.java
+++ src/Database/Object/User.java
@@ -17,7 +17,7 @@ public class User {
     public User(String fullName, String email,Singleton_Login login,Address address,boolean Registered, Date dateOfBirth, String phoneNum, Address address) {
         this.fullName = fullName;
         this.email = email;
-        this.login = new login;
+        this.login = login;
         this.address = address;
         this.Registered = Registered;
         this.dateOfBirth = dateOfBirth;
@@ -25,7 +25,63 @@ public class User {
     
     } 
     
+    
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLogin(Singleton_Login login) {
+        this.login = login;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public void setRegistered(boolean registered) {
+        this.registered = registered;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    // Getters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Singleton_Login getLogin() {
+        return login;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public boolean isRegistered() {
+        return registered;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
 
 
 }
